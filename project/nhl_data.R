a8149d14ca4ca25521334b39f4648e475c806ad7
Ryan Elmore <Ryan.Elmore@du.edu>
2022-02-09 09:13:17 -0700

    grabbed the actual results

--- project/nhl_data.R
+++ project/nhl_data.R
@@ -28,3 +28,25 @@ for (i in seq_along(files_skaters)){
 
 write.csv(df_goalies, file = "project/data/goalies.csv", row.names = F)
 write.csv(result, file = "project/data/skaters.csv", row.names = F)
+
+## Actual Data
+files_goalies <- files[grepl("goalies-actual", files)]
+df_goalies <- readxl::read_xlsx(paste("project/data/", files_goalies, sep = "")) %>% 
+  janitor::clean_names()
+write.csv(df_goalies, file = "project/data/goalies-actual.csv", row.names = F)
+
+files_skaters <- files[grepl("skaters-actual", files)]
+
+for (i in seq_along(files_skaters)){
+  dat <- readxl::read_xlsx(paste("project/data/", 
+                                 files_skaters[i], sep = "")) %>% 
+    janitor::clean_names() %>% 
+    dplyr::mutate(s_percent = as.numeric(s_percent),
+                  fow_percent = as.numeric(fow_percent))
+  print(i)
+  if(exists("result")){
+    result <- dplyr::bind_rows(result, dat)
+  } else result <- dat
+}
+
+write.csv(result, file = "project/data/skaters-actual.csv", row.names = F)
